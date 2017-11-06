b0be19f42c68cafa1fdd96974615231a9f531a5e
James <842324595@qq.com>
2017-11-06 17:17:44 +0800

    完成天气的手动刷新，城市的切换，后台自动更新天气和必应一图

--- app/src/main/java/com/zqy/weatherdemo/WeatherActivity.java
+++ app/src/main/java/com/zqy/weatherdemo/WeatherActivity.java
@@ -2,6 +2,8 @@ package com.zqy.weatherdemo;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
@@ -71,6 +73,11 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decoView = getWindow().getDecorView();
+            decoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         ButterKnife.bind(this);
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
